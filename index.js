a99826d1c336f6c20c923521074222dd87903b33
Brandon Richardson <brandon1024.br@gmail.com>
2017-04-08 17:20:54 -0300

    More changes

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
+var occurrenceNumber = 0;
+var totalOccurrences = 0;
+
 window.onload = function() {
-    document.getElementById('searchNextBtn').onclick = searchText;
+    document.getElementById('searchField').oninput = update;
+    document.getElementById('searchNextBtn').onclick = highlightFirst;
     document.getElementById('closeBtn').onclick = closeExtension;
 };
 
-function searchText() {
-    var search = document.getElementById('searchField').value;
-    if(search) {
-        chrome.tabs.query({active:true,currentWindow:true},function(tabs) {
-            window.alert('sup')
-        });
+function update() {
+    highlightAll();
+    updateOccurrencesText();
+}
+
+function highlightAll() {
+    var regex = document.getElementById('searchField').value;
+    var elements = document.getElementsByTagName('*');
+
+    for(index = 0; index < elements.length; index++) {
+        var element = elements[index];
+
+        for(childIndex = 0; childIndex < element.childNodes.length; childIndex++) {
+            var node = element.childNodes[childIndex];
+
+            if(node.nodeType === 3 && node.nodeValue === regex) {
+                node.nodeValue = "<span style=\"background-color: #FFFF00\">" + node.nodeValue + "</span>";
+            }
+        }
     }
+}
+
+function highlightFirst() {
+
+}
+
+function updateOccurrencesText() {
+    document.getElementById('occurrences').innerHTML = occurrenceNumber + "/" + totalOccurrences;
+}
+
+function searchPageForRegex() {
+    //stub
 }
 
 function closeExtension() {
