696a515a5002eb61525a0f673f1f2d43fa410c80
Brandon Richardson <brandon1024.br@gmail.com>
2017-04-08 16:32:46 -0300

    Further improvements

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 window.onload = function() {
     document.getElementById('searchNextBtn').onclick = searchText;
+    document.getElementById('closeBtn').onclick = closeExtension;
 };
+
 function searchText() {
     var search = document.getElementById('searchField').value;
     if(search) {
@@ -8,4 +10,8 @@ function searchText() {
             window.alert('sup')
         });
     }
+}
+
+function closeExtension() {
+    window.close();
 }
