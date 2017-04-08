a479225e416931df740769f768d58b8b40bbb4bc
Brandon Richardson <brandon1024.br@gmail.com>
2017-04-08 16:13:15 -0300

    baseline

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-window.onload = function(){
-    document.getElementById('searchButton').onclick = searchText;
+window.onload = function() {
+    document.getElementById('SearchNextBtn').onclick = searchText;
 };
-
-function searchText(){
-    var regex = document.getElementById('searchText').value;
-    if(regex){
-        chrome.tabs.query({active: true,currentWindow: true},function(tabs){
-            chrome.tabs.executeScript(tabs[0].id,{file: search.js});
-            chrome.tabs.sendMessage(tabs[0].id,{method: 'search', regex: regex});
+function searchText() {
+    var search = document.getElementById('SearchField').value;
+    if(search) {
+        chrome.tabs.query({active:true,currentWindow:true},function(tabs) {
+            window.alert('sup')
         });
     }
 }
