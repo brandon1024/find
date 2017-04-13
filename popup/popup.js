aa0d030012da107998ca3cf8f1a62f20ad0313cf
MikeWalz11 <michaela.walz@yahoo.com>
2017-04-12 22:07:55 -0300

    Added ability to remember previous search, and highlight search field on popup open

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,45 @@
+
 window.onload = function() {
+    retrieveLastSearch();
+
     document.getElementById('closeBtn').onclick = closeExtension;
+    document.getElementById('searchField').addEventListener('keyup', storeSearchEntry);
 };
 
 function closeExtension() {
     window.close();
+}
+
+/**
+ * retrieves current text from search field and stores it.
+ */
+function storeSearchEntry() {
+    var inputVal =  document.getElementById('searchField').value;
+
+    chrome.storage.sync.set({'previousSearch': inputVal}, function() {
+        console.log('Previous search: \"' + inputVal + '\" saved');
+    });
+}
+
+/**
+ * retrieves previous search term from storage and sets is as current
+ * value for search field.
+ */
+function retrieveLastSearch(callback) {
+    console.log("Attempting to retrieve last search field entry.");
+
+    chrome.storage.sync.get('previousSearch', function (obj) {
+        console.log(obj);
+        console.log('Setting input field value to: \"' + obj['previousSearch'] + '\"');
+        document.getElementById('searchField').value = obj['previousSearch'];
+        inputSelect();
+    });
+}
+
+/**
+ * selects the contects of the search field.
+ */
+function inputSelect() {
+    console.log("Selecting contents of Search Field");
+    document.getElementById("searchField").select();
 }
