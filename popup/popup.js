87e7a401e25dcc32611d90fc57373610edd4b9b8
Brandon Richardson <brandon1024.br@gmail.com>
2017-09-08 21:10:58 -0300

    popup script refactors

--- popup/popup.js
+++ popup/popup.js
@@ -10,35 +10,50 @@ window.onload = function addListeners() {
     document.getElementById('close-button').addEventListener('click', closeExtension);
     document.getElementById('search-field').addEventListener('input', updateHighlight);
     document.getElementById('search-field').addEventListener('input', updateLocalStorage);
-    document.getElementById('search-field').addEventListener('keyup', handleKeyPress, true);
     document.getElementsByClassName('max-results-slider')[0].addEventListener('input', updateMaxHighlights);
 
     document.body.addEventListener('click', function(){
         document.getElementById('search-field').focus();
     });
 
-    chrome.tabs.query({'active': true, currentWindow: true}, function (tabs) {
-        var url = tabs[0].url;
-        if(!(url.match(/chrome:\/\/newtab\//)) && (url.match(/chrome:\/\/.*/) || url.match(/https:\/\/chrome.google.com\/webstore\/.*/))) {
-            document.getElementById('extension-message-body').style.display = 'initial';
-            document.getElementById('extension-limitation-chrome-settings-text').style.display = 'initial';
+    document.getElementById('search-field').addEventListener('keyup', function(e){
+        if (e.keyCode == 13 && e.shiftKey)
+            previousHighlight();
+        else if (e.keyCode == 27 || e.keyCode == 13 && e.ctrlKey)
+            closeExtension();
+        else if (e.keyCode == 13)
+            nextHighlight();
+        else if(e.keyCode == 79 && e.ctrlKey) {
+            var el = document.getElementById("regex-options");
+            if(el.style.display == 'none' || el.style.display == '')
+                el.style.display = 'inherit';
+            else
+                el.style.display = 'none';
         }
-        else {
-            chrome.tabs.executeScript( {
-                code: "window.getSelection().toString();"
-            }, function(selection) {
-                var selectedText = selection[0];
-                if(selectedText === undefined || selectedText == null || selectedText.length <= 0) {
-                    retrieveLastSearch();
-                }
-                else {
-                    changeSearchFieldText(selection[0]);
-                    updateHighlight();
-                }
-            });
-        }
-    });
+    }, true);
 };
+
+chrome.tabs.query({'active': true, currentWindow: true}, function (tabs) {
+    var url = tabs[0].url;
+    if(!(url.match(/chrome:\/\/newtab\//)) && (url.match(/chrome:\/\/.*/) || url.match(/https:\/\/chrome.google.com\/webstore\/.*/))) {
+        document.getElementById('extension-message-body').style.display = 'initial';
+        document.getElementById('extension-limitation-chrome-settings-text').style.display = 'initial';
+    }
+    else {
+        chrome.tabs.executeScript( {
+            code: "window.getSelection().toString();"
+        }, function(selection) {
+            var selectedText = selection[0];
+            if(selectedText === undefined || selectedText == null || selectedText.length <= 0) {
+                retrieveLastSearch();
+            }
+            else {
+                changeSearchFieldText(selection[0]);
+                updateHighlight();
+            }
+        });
+    }
+});
 
 //Listen for messages from the background script
 port.onMessage.addListener(function listener(response) {
@@ -47,23 +62,23 @@ port.onMessage.addListener(function listener(response) {
         updateIndexText(response.index, response.total);
 
         if(response.index == 0 && response.total == 0)
-            disableButtons();
+            enableButtons(false);
         else
-            enableButtons();
+            enableButtons(true);
     }
     else if(response.action == 'empty_regex') {
         showMalformedRegexIcon(false);
         updateIndexText();
-        disableButtons();
+        enableButtons(false);
     }
     else if(response.action == 'invalid_regex') {
         updateIndexText();
-        disableButtons();
+        enableButtons(false);
         showMalformedRegexIcon(true);
     }
     else {
         console.error('Unrecognized action:', response.action);
-        disableButtons();
+        enableButtons(false);
     }
 });
 
@@ -73,7 +88,7 @@ function updateHighlight() {
     
     var regex = getSearchFieldText();
     var action = 'update';
-    invokeAction({action: action, regex: regex});
+    port.postMessage({action: action, regex: regex});
 }
 
 //Highlight next occurrence of regex
@@ -84,7 +99,7 @@ function nextHighlight() {
     }
 
     var action = 'next';
-    invokeAction({action: action});
+    port.postMessage({action: action});
     document.getElementById('search-field').focus();
 }
 
@@ -96,12 +111,8 @@ function previousHighlight() {
     }
 
     var action = 'previous';
-    invokeAction({action: action});
+    port.postMessage({action: action});
     document.getElementById('search-field').focus();
-}
-
-function invokeAction(params) {
-    port.postMessage(params);
 }
 
 //Close the extension
@@ -110,29 +121,9 @@ function closeExtension() {
     window.close();
 }
 
-//Handles keyboard shortcuts
-function handleKeyPress(e) {
-    if (e.keyCode == 13 && e.shiftKey) {
-        previousHighlight();
-    }
-    else if (e.keyCode == 13 && e.ctrlKey) {
-        closeExtension();
-    }
-    else if(e.keyCode == 79 && e.ctrlKey) {
-        var el = document.getElementById("regex-options");
-        if(el.style.display == 'none' || el.style.display == '')
-            el.style.display = 'inherit';
-        else
-            el.style.display = 'none';
-    }
-    else if (e.keyCode == 13) {
-        nextHighlight();
-    }
-}
-
 //Converts current search field text to JSON payload and send to storeDataToLocalStorage() to store
 function updateLocalStorage() {
-    var payload = {'previousSearch': document.getElementById('search-field').value};
+    var payload = {'previousSearch': getSearchFieldText()};
     storeDataToLocalStorage(payload);
 }
 
@@ -181,27 +172,25 @@ function updateIndexText() {
 }
 
 function updateMaxHighlights() {
-    document.getElementsByClassName('max-results-slider-value')[0].innerText = document.getElementsByClassName('max-results-slider')[0].value;
+    var rangeValue = document.getElementsByClassName('max-results-slider')[0].value;
+    document.getElementsByClassName('max-results-slider-value')[0].innerText = rangeValue == 500 ? '∞' : rangeValue;
 }
 
 //Show or hide red exclamation icon in the extension popup
 function showMalformedRegexIcon(flag) {
-    if(flag)
-        document.getElementById('invalid-regex-icon').style.display = 'initial';
-    else
-        document.getElementById('invalid-regex-icon').style.display = 'none';
+    document.getElementById('invalid-regex-icon').style.display = flag ? 'initial' : 'none';
 }
 
 //Enable next and previous buttons
 function enableButtons() {
+    if(arguments.length == 1 && !arguments[0]) {
+        document.getElementById('search-prev-button').disabled = true;
+        document.getElementById('search-next-button').disabled = true;
+        return;
+    }
+
     document.getElementById('search-prev-button').disabled = false;
     document.getElementById('search-next-button').disabled = false;
-}
-
-//Disable next and previous buttons
-function disableButtons() {
-    document.getElementById('search-prev-button').disabled = true;
-    document.getElementById('search-next-button').disabled = true;
 }
 
 //Formats numbers to have thousands comma delimiters
