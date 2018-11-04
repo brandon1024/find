04dc942aab47a5f623542b18d4c29c713bf37d4f
Brandon Richardson <brandon1024.br@gmail.com>
2018-11-04 10:49:03 -0400

    initial changes

---
+++ popup/js/i18n.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * Create the Popup BackgroundProxy namespace.
+ * */
+Find.register('Popup.BackgroundProxy', function (self) {
+
+    /**
+     *
+     * */
+    self.init = function() {
+
+    }
+});
