e4c89101330719faeff065d25e63422c76563b37
Andrew Rogl <arogl1@gmail.com>
2026-08-09 22:22:05 +1000

    fix return JSDoc popup/js/storage.js

--- popup/js/storage.js
+++ popup/js/storage.js
@@ -124,7 +124,7 @@ Find.register('Popup.Storage', function (self) {
      *
      * @private
      * @param {string} key - The key associated with the data being retrieved.
-     * @param {function} callback - The callback function that will accept the data.
+     * @return {function} callback - The callback function that will accept the data.
      * */
     function retrieve(key, callback) {
         if (locked) {
