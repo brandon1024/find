662d1d364f3a80561be25e93395d9a730214afb3
Andrew Rogl <arogl1@gmail.com>
2026-08-09 19:50:23 +1000

    update popup/js/message-pane.js

--- popup/js/message-pane.js
+++ popup/js/message-pane.js
@@ -8,15 +8,15 @@ Find.register('Popup.MessagePane', function (self) {
     /**
      * Display an error message that indicates that the current URL is forbidden.
      * */
-    self.showInternalRestrictedBrowserPageErrorMessage = function() {
+    self.showInternalRestrictedBrowserPageErrorMessage = function () {
         document.getElementById('extension-message-body').style.display = 'initial';
         document.getElementById('extension-limitation-internal-restricted-browser-page-text').style.display = 'initial';
-    }
+    };
 
     /**
      * Display an error message that indicates that the current page cannot be parsed.
      * */
-    self.showPDFSearchErrorMessage = function() {
+    self.showPDFSearchErrorMessage = function () {
         document.getElementById('extension-message-body').style.display = 'initial';
         document.getElementById('extension-limitation-pdf-fileview-text').style.display = 'initial';
     };
@@ -24,7 +24,7 @@ Find.register('Popup.MessagePane', function (self) {
     /**
      * Display an error message that indicates that the current page cannot be parsed.
      * */
-    self.showOfflineFileErrorMessage = function() {
+    self.showOfflineFileErrorMessage = function () {
         document.getElementById('extension-message-body').style.display = 'initial';
         document.getElementById('extension-limitation-offline-file-search-text').style.display = 'initial';
     };
